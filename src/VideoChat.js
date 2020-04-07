19214951afea967bd43e92baa8340e1e37ecb028
Misaki Shioi <shioi.mm@gmail.com>
2020-04-07 12:03:22 +0900

    Use useState & Add functions
    - useState to update states of username/roomName
    - finctions to set username/roomName

--- src/VideoChat.js
+++ src/VideoChat.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const VideoChat = () => {
+  const [username, setUsername] = useState('');
+  const [roomName, setRoomName] = useState('');
+  const [token, setToken] = useState(null);
+
+  const handleUsernameChange = event => {
+    setUsername(event.target.value);
+  };
+
+  const handleRoomNameChange = event => {
+    setRoomName(event.target.value);
+  };
+
   return <div></div> // we'll build up our response later
 };
 
