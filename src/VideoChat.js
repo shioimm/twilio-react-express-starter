3be923f582edf2da780f7b6398822197331c67b7
Misaki Shioi <shioi.mm@gmail.com>
2020-04-07 12:16:19 +0900

    Add handleLogout to delete token and logout.

--- src/VideoChat.js
+++ src/VideoChat.js
@@ -28,6 +28,10 @@ const VideoChat = () => {
     setToken(data.token);
   }, [username, roomName]);
 
+  const handleLogout = useCallback(event => {
+    setToken(null);
+  }, []);
+
   return <div></div> // we'll build up our response later
 };
 
