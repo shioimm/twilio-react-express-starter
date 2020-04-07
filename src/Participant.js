48f48b6f0b944ecb3f1ce10bf483378fa541cd90
Misaki Shioi <shioi.mm@gmail.com>
2020-04-07 16:28:43 +0900

    Add Participant component
    to handle video & audio of user

---
+++ src/Participant.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const Participant = ({ participant }) => {
+  const [videoTracks, setVideoTracks] = useState([]);
+  const [audioTracks, setAudioTracks] = useState([]);
+};
+
+export default Participant;
