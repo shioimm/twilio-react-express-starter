9af85d89f3ea5daebe52935f29c954cc8280432b
Misaki Shioi <shioi.mm@gmail.com>
2020-04-07 16:30:43 +0900

    Use useRef & Add JSX to Participant component
    - useRef to access DOM from React
    - JSX to play medias

--- src/Participant.js
+++ src/Participant.js
@@ -3,6 +3,18 @@ import React, { useState, useEffect, useRef } from 'react';
 const Participant = ({ participant }) => {
   const [videoTracks, setVideoTracks] = useState([]);
   const [audioTracks, setAudioTracks] = useState([]);
+
+  const videoRef = useRef();
+  const audioRef = useRef();
+
+  return (
+    <div className="participant">
+      <h3>{participant.identity}</h3>
+      <video ref={videoRef} autoPlay={true} />
+      <audio ref={audioRef} autoPlay={true} muted={true} />
+    </div>
+  );
+ });
 };
 
 export default Participant;
