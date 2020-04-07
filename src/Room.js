d8909f224223ab476afada365d5429601b74f636
Misaki Shioi <shioi.mm@gmail.com>
2020-04-07 13:47:58 +0900

    Use useState & Add function to Room component
    - useState to update states of room/participants
    - finction to get identities of remote participants

--- src/Room.js
+++ src/Room.js
@@ -2,7 +2,12 @@ import React, { useState, useEffect } from 'react';
 import Video from 'twilio-video';
 
 const Room = ({ roomName, token, handleLogout }) => {
+  const [room, setRoom] = useState(null);
+  const [participants, setParticipants] = useState([]);
 
+  const remoteParticipants = participants.map(participant => (
+    <p key={participant.sid}>participant.identity</p>
+  ));
 });
 
 export default Room;
